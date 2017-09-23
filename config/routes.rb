742a916111a31477a7a9e1393742721b6a2ddc9a
Derek Devera <ddevera@g.clemson.edu>
2017-09-23 00:44:30 +0000

    rails hw part 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  get '/' => 'movies#index'
   resources :movies
   
   # Example resource route with options:
